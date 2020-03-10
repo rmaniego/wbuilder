a442544e8ce0598f4db8e6fcefbd05ef96671e43
Rodney Maniego Jr <rodney.maniego@lexmark.com>
2020-03-10 17:08:23 +0800

    Fixed set method.

--- wbuilder/wbuilder.py
+++ wbuilder/wbuilder.py
@@ -328,7 +328,7 @@ class Blocks:
         self.depth = depth(self.depth, -1)
         return self
     
-    def set(self, key, value):
+    def set_(self, key, value):
         tabs = ("\t" * (self.depth + 1))
         self.statements.append(concat([tabs, "{%- set ", key, " = '", value, "' -%}"]))
         return self
